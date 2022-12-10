91f4c6d05340fa7c9124e63e19023e06c5add810
bhupendra <bhupendrayadav2732@gmail.com>
2022-12-10 16:03:42 +0530

    newFile

---
+++ 17_Bulb_Project/bulb_index.js
@@ -0,0 +1,42 @@
+
+    const turnOn=()=>
+    {//bulb on
+        let imgOb=document.getElementById('bulb');
+        imgOb.setAttribute("src","bulb_ON.jpg");
+
+    };
+
+    function turnOFF()
+    {
+        let imgOb=document.getElementById('bulb');
+        imgOb.setAttribute("src","bulb_OFF.jpg");
+
+    };
+
+    //toggle
+    const toggleBulb=()=>
+    {
+        let imgOb = document.getElementById('bulb');
+        let srcValue=imgOb.getAttribute("src");
+        //console.log(srcValue);
+
+        let buttonob = document.getElementById('onOFFbutton'); //cahnge the button value on of
+        let buttonText = buttonob.innerHTML;
+
+
+        if(srcValue=='bulb_OFF.jpg')
+        {
+            turnOn();
+            buttonob.innerHTML="OFF"
+        }
+        else
+        {
+            turnOFF();
+            buttonob.innerHTML="ON";
+
+        }
+        document.body.style.backgroundColor="gray";
+    };
+   
+   
+
